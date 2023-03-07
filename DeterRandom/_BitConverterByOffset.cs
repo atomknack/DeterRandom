ad705b91a36f4e38cbf93ebf7744a772bcf251d2
atomknack <110350925+atomknack@users.noreply.github.com>
2023-03-08 02:53:10 +0400

    Create _BitConverterByOffset.cs
    Maybe sometime in future make offset type converter

---
+++ DeterRandom/_BitConverterByOffset.cs
@@ -0,0 +1,43 @@
+﻿/*
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace DeterRandom
+{
+    [StructLayout(LayoutKind.Explicit)]
+    public readonly struct TypeOffsetConverter_64_32
+    {
+        [FieldOffset(0)]
+        public readonly ulong _0_ulong;
+        [FieldOffset(0)]
+        public readonly long _0_long;
+        [FieldOffset(0)]
+        public readonly double _0_double;
+        [FieldOffset(0)]
+        public readonly uint _0_uint;
+        [FieldOffset(0)]
+        public readonly int _0_int;
+        [FieldOffset(0)]
+        public readonly float _0_float;
+        [FieldOffset(4)]
+        public readonly int _1_uint;
+        [FieldOffset(4)]
+        public readonly int _1_int;
+        [FieldOffset(4)]
+        public readonly float _1_float;
+
+        public TypeOffsetConverter_64_32(ulong @ulong)
+        {
+            _0_ulong = @ulong;
+            Unsafe.SkipInit(out _0_long);
+            Unsafe.SkipInit(out _0_double);
+            Unsafe.SkipInit(out _0_uint);
+            Unsafe.SkipInit(out _0_int);
+            Unsafe.SkipInit(out _0_float);
+            Unsafe.SkipInit(out _1_uint);
+            Unsafe.SkipInit(out _1_int);
+            Unsafe.SkipInit(out _1_float);
+        }
+    }
+}
+*/
